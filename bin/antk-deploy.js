07345426e8161bd97f93204aaa5b382cfea25651
Olivier Van Aken <olivier.va@outlook.com>
2019-07-09 12:49:40 +0200

    added more options - pass on options to deploy command

--- bin/antk-deploy.js
+++ bin/antk-deploy.js
@@ -9,8 +9,12 @@ const { deploy } = require('../src/commands/deploy');
 program.name('antk deploy');
 program.usage('[options]');
 
-program.action(async (cmd) => {
-    await deploy();
+program.option('-e, --env, --environment <name>');
+program.option('-r, --region <name>');
+program.option('-s, --stack <name>');
+
+program.action(async (command) => {
+    await deploy(command.env, command.region, command.stack);
 });
 
 program.parse(process.argv);
--- src/commands/deploy/index.js
+++ src/commands/deploy/index.js
@@ -1,12 +1,21 @@
 'use strict';
 
-async function deploy() {
+
+
+async function deploy(env, region, stack) {
     // read the toolkit.yml file
     // parse project properties, e.g. project name
     // parse variables
     // deploy artifact bucket
     // execute all separate deploy steps
-    console.info('deployeru')
+
+    if (stack) {
+        // read specific stack config
+        // deploy stack
+    } else {
+        // read whole config
+        // deploy all stacks in order
+    }
 }
 
 module.exports.deploy = deploy;
