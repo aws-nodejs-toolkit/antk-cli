ff9792de737ceebd3380fc4e0a7aee24f94120f7
Olivier Van Aken <olivier.va@outlook.com>
2019-07-28 13:40:15 +0200

    applied variable replacements to yaml object

--- src/config/parser.js
+++ src/config/parser.js
@@ -5,8 +5,8 @@ const variables = require('./variables');
 const _yaml = require('../yaml');
 
 async function parse(path, options) {
-    const content = await reader.read(path);
-    const yaml = _yaml.parse(content);
+    const file = await reader.read(path);
+    let yaml = _yaml.parse(file);
     const vars = variables.collect(yaml);
 
     const variableMap = {};
@@ -14,19 +14,12 @@ async function parse(path, options) {
     for (const variable of vars) {
         const [type, ...key] = variables.parse(variable);
         const result = await variables.resolve(type, key, options).catch((err) => {
-            console.log(err);
+            // console.log(err);
         });
-        // if matches ${} continue
-        console.log(result);
         variableMap[variable] = result;
     }
 
-    // for each var -> resolve
-    // get map of var:resolved value back
-    // apply map to yaml, replace method?
-
-    // yaml = resolved variables and all blabla,
-
+    yaml = variables.replace(yaml, variableMap);
 
     return yaml;
 }
--- src/config/variables/index.js
+++ src/config/variables/index.js
@@ -2,8 +2,10 @@
 
 const { collect } = require('./collector');
 const { parse } = require('./parser');
+const { replace } = require('./replacer');
 const { resolve } = require('./resolver');
 
 module.exports.collect = collect;
 module.exports.parse = parse;
+module.exports.replace = replace;
 module.exports.resolve = resolve;
